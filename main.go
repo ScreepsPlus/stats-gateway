4baf2fd49da1cd29386d92ca146a3a820e86eaae
Adam Shumann <ashumann13@gmail.com>
2017-08-25 15:54:52 -0500

    Patched stats key leak

--- main.go
+++ main.go
@@ -20,77 +20,90 @@ func main() {
 	url, _ := url.Parse(backendUrl)
 	proxy := httputil.NewSingleHostReverseProxy(url)
 	h := http.NewServeMux()
-	h.Handle("/metrics/find/",Find(proxy))
-	h.Handle("/render",Render(proxy))
-    log.Fatal(http.ListenAndServe(":8181", h))
+	h.Handle("/metrics/find/", Find(proxy))
+	h.Handle("/render", Render(proxy))
+	log.Fatal(http.ListenAndServe(":8181", h))
 }
 
 func Find(next http.Handler) http.Handler {
-    ourFunc := func(w http.ResponseWriter, r *http.Request) {
-    	query := r.URL.Query().Get("query")
-    	if query == "*" {
-    		w.Header().Add("Content-Type","application/json")
-    		w.WriteHeader(200)
-    		b := MetricMap([]string{"screeps"}, "")
-    		w.Write(b)
-    		return
-    	}
-    	cookie := r.Header.Get("Cookie")
-    	orgs, err := GetOrgs(cookie)
-    	if err != nil {
+	ourFunc := func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query().Get("query")
+		if query == "*" || query == "screeps" {
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(200)
+			b := MetricMap([]string{"screeps"}, "")
+			w.Write(b)
+			return
+		}
+		cookie := r.Header.Get("Cookie")
+		orgs, err := GetOrgs(cookie)
+		if err != nil {
 			fmt.Printf("%+v\n", err)
 			w.WriteHeader(500)
 			return
 		}
-    	if query == "screeps.*" || strings.HasPrefix(query, "screeps.*") {
-    		acl := GetACL(orgs)
-    		query = strings.Replace(query, "*", acl, 1)
-    		vals := r.URL.Query()
-    		vals.Set("query", query)
-    		r.URL.RawQuery = vals.Encode()
-    	}
-    	next.ServeHTTP(w, r)
-    }
-    return http.HandlerFunc(ourFunc)
+        valid := false
+        for _, org := range orgs {
+            val := fmt.Sprintf("screeps.%s", org.Name)
+            if strings.HasPrefix(query, val) {
+                valid = true
+                break
+            }
+        }
+		if query == "screeps.*" || strings.HasPrefix(query, "screeps.*") {
+			acl := GetACL(orgs)
+			query = strings.Replace(query, "*", acl, 1)
+			vals := r.URL.Query()
+			vals.Set("query", query)
+			r.URL.RawQuery = vals.Encode()
+            valid = true
+		}
+        if valid {
+		  next.ServeHTTP(w, r)
+        } else {
+          w.WriteHeader(403)
+        }
+	}
+	return http.HandlerFunc(ourFunc)
 }
 
 func Render(next http.Handler) http.Handler {
-    ourFunc := func(w http.ResponseWriter, r *http.Request) {
-    	cookie := r.Header.Get("Cookie")
-    	orgs, err := GetOrgs(cookie)
-    	if err != nil {
+	ourFunc := func(w http.ResponseWriter, r *http.Request) {
+		cookie := r.Header.Get("Cookie")
+		orgs, err := GetOrgs(cookie)
+		if err != nil {
 			fmt.Printf("%+v\n", err)
 			w.WriteHeader(500)
 			return
 		}
-		
-    	acl := GetACL(orgs)
-    	r.ParseForm()
-    	targets := r.PostForm["target"]
-    	validTargets := make([]string,0)
-    	for _, target := range targets {
-    		valid := false
-    		for _, org := range orgs {
-    			val := fmt.Sprintf("screeps.%s", org.Name)
-    			if strings.Contains(target, val) {
-    				valid = true
-    				break
-    			}
-    		}
-    		if !valid && strings.Contains(target, "screeps.*") {
-    			target = strings.Replace(target, "screeps.*", fmt.Sprintf("screeps.%s", acl), 10)
-    			valid = true
-    		}
-    		if valid {
-    			validTargets = append(validTargets, target)
-    		}    		
-    	}
-    	r.PostForm["target"] = validTargets
-    	body := r.PostForm.Encode()
-    	r.Body = ioutil.NopCloser(bytes.NewBufferString(body))
-    	r.ContentLength = int64(len(body))
-    	next.ServeHTTP(w, r)
-    }
+
+		acl := GetACL(orgs)
+		r.ParseForm()
+		targets := r.PostForm["target"]
+		validTargets := make([]string, 0)
+		for _, target := range targets {
+			valid := false
+			for _, org := range orgs {
+				val := fmt.Sprintf("screeps.%s", org.Name)
+				if strings.Contains(target, val) {
+					valid = true
+					break
+				}
+			}
+			if !valid && strings.Contains(target, "screeps.*") {
+				target = strings.Replace(target, "screeps.*", fmt.Sprintf("screeps.%s", acl), -1)
+				valid = true
+			}
+			if valid {
+				validTargets = append(validTargets, target)
+			}
+		}
+		r.PostForm["target"] = validTargets
+		body := r.PostForm.Encode()
+		r.Body = ioutil.NopCloser(bytes.NewBufferString(body))
+		r.ContentLength = int64(len(body))
+		next.ServeHTTP(w, r)
+	}
 	return http.HandlerFunc(ourFunc)
 }
 
@@ -100,17 +113,17 @@ func GetACL(orgs []GrafanaOrganization) string {
 		list = append(list, org.Name)
 	}
 	s := strings.Join(list, ",")
-	s = fmt.Sprintf("{%s}",s)
+	s = fmt.Sprintf("{%s}", s)
 	return s
 }
 
 func GetOrgs(cookie string) ([]GrafanaOrganization, error) {
-	req, _ := http.NewRequest("GET", grafanaUrl + "/api/user/orgs", nil)
+	req, _ := http.NewRequest("GET", grafanaUrl+"/api/user/orgs", nil)
 	req.Header.Add("Cookie", cookie)
 	var client http.Client
 	res, _ := client.Do(req)
 	defer res.Body.Close()
-	orgs := make([]GrafanaOrganization,0)
+	orgs := make([]GrafanaOrganization, 0)
 	err := json.NewDecoder(res.Body).Decode(&orgs)
 	return orgs, err
 }
@@ -135,15 +148,15 @@ func MetricMap(list []string, base string) []byte {
 }
 
 type GraphiteMetric struct {
-	AllowChildren int `json:"allowChildren"`
-	Expandable int `json:"expandable"`
-	Id string `json:"id"`
-	Leaf int `json:"leaf"`
-	Text string `json:"text"`
+	AllowChildren int    `json:"allowChildren"`
+	Expandable    int    `json:"expandable"`
+	Id            string `json:"id"`
+	Leaf          int    `json:"leaf"`
+	Text          string `json:"text"`
 }
 
 type GrafanaOrganization struct {
 	OrgId int
-	Name string
-	Role string
+	Name  string
+	Role  string
 }
